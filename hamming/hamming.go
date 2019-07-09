89a6e279dbe9e18bc794891053a304c4e1d5f97f
m.desmarais@f5.com <m.desmarais@f5.com>
2019-07-09 13:21:47 -0400

    Implement pack hamming in order to provide helper functions to calculate the distance between two DNA strands

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,26 @@
+/*
+Package hamming implements helpers for the DNS strand problem
+*/
+
 package hamming
 
+import "errors"
+
+// Distance compares a and b to get the Hamming distance between these two DNA strands.
 func Distance(a, b string) (int, error) {
+	//Empty Strand
+	if a == "" && b == "" {
+		return 0, nil
+	}
+
+	if (len(a) > 0 && len(b) > 0) && (len(a) == len(b)) {
+		d := 0 //holds the current distant count
+		for i := 0; i < len(a); i++ {
+			if a[i] != b[i] {
+				d++
+			}
+		}
+		return d, nil
+	}
+	return 0, errors.New("error")
 }
